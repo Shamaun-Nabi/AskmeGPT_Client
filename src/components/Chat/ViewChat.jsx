7d9ec9716749c37b0cca0b35d3d330ddeb40166d
Shamaun-Nabi <snashik950@gmail.com>
2023-01-31 03:38:25 +0600

    adding build

--- src/components/Chat/ViewChat.jsx
+++ src/components/Chat/ViewChat.jsx
@@ -16,7 +16,7 @@ function ViewChat({ userChat, loading }) {
   }, [parent]);
 
   const aiStyle =
-    "max-w-[80%]  bg-slate-600 text-white break-words mr-auto px-4 py-2  rounded-bl-none rounded-md  overflow-auto text-justify";
+    "max-w-[80%]  bg-slate-600 text-white break-words mr-auto px-4 py-2  rounded-bl-none rounded-md overflow-auto text-justify";
   return (
     <>
       <div className="flex flex-col gap-4 p-8 md:p-20" ref={parent}>
